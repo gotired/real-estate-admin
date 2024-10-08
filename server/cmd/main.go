ddc04e6f6fccd6e48caa1b7e57fcdd7c2377eb56
gotired <gotired@hotmail.com>
2024-10-08 23:08:13 +0700

    feat: setup main app with user router

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -1,5 +1,27 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
 
+	"github.com/gotired/real-estate-admin/server/internal/domain/service"
+	"github.com/gotired/real-estate-admin/server/internal/infrastructure/persistance/user"
+	"github.com/gotired/real-estate-admin/server/internal/infrastructure/server"
+	"github.com/gotired/real-estate-admin/server/internal/interface/api/handler"
+	"github.com/gotired/real-estate-admin/server/internal/interface/api/router"
+)
+
+func main() {
+	app, config, db := server.Setup()
+
+	userRepo := user.New(db)
+	userService := &service.User{Repository: userRepo}
+	userHandler := &handler.User{Service: userService}
+
+	apiRouter := app.Group("/api")
+	v1Router := apiRouter.Group("/v1")
+
+	router.SetupUserRouter(v1Router, userHandler)
+
+	log.Fatal(app.Listen(fmt.Sprintf(":%s", config.APP.Port)))
 }
