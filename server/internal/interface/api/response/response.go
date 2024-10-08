be3b6c01c614ba53b4cf82a1eaebc2d6d7db7dd0
gotired <gotired@hotmail.com>
2024-10-08 23:10:22 +0700

    refactor: merge created and success response in to 1 module

--- server/internal/interface/api/response/response.go
+++ server/internal/interface/api/response/response.go
@@ -5,18 +5,10 @@ import (
 	"github.com/gotired/real-estate-admin/server/internal/domain/model"
 )
 
-func Success(c *fiber.Ctx, data interface{}) error {
-	return c.Status(fiber.StatusOK).JSON(model.Response{
+func Success(c *fiber.Ctx, statusCode int, data interface{}) error {
+	return c.Status(statusCode).JSON(model.Response{
 		Status: "success",
 		Detail: "",
-		Data:   data,
-	})
-}
-
-func Created(c *fiber.Ctx, data interface{}, detail string) error {
-	return c.Status(fiber.StatusCreated).JSON(model.Response{
-		Status: "success",
-		Detail: detail,
 		Data:   data,
 	})
 }
