2b08c7933221472f4468c395a6aecbb05d3d0cce
gotired <gotired@hotmail.com>
2024-10-14 14:41:37 +0700

    feat: update user handler

--- server/internal/interface/api/handler/user.go
+++ server/internal/interface/api/handler/user.go
@@ -9,15 +9,32 @@ import (
 )
 
 type User struct {
-	Service *service.User
+	UserService     *service.User
+	PropertyService *service.Property
 }
 
 func (u *User) List(c *fiber.Ctx) error {
-	users, err := u.Service.List()
+	skip, err := c.ParamsInt("skip", 0)
+	if err != nil {
+		return response.Error(c, 400, err.Error())
+	}
+	limit, err := c.ParamsInt("limit", 10)
+	if err != nil {
+		return response.Error(c, 400, err.Error())
+	}
+
+	if limit < 0 {
+		return response.Error(c, 400, "parameter 'limit' must be greater than 0")
+	}
+	if skip < 0 {
+		return response.Error(c, 400, "parameter 'skip' must be greater than 0")
+	}
+
+	users, err := u.UserService.List(&skip, &limit)
 	if err != nil {
 		return response.Error(c, 500, err.Error())
 	}
-	return response.Success(c, 200, users)
+	return response.Success(c, 200, users, "")
 }
 
 func (u *User) Get(c *fiber.Ctx) error {
@@ -28,11 +45,17 @@ func (u *User) Get(c *fiber.Ctx) error {
 		return response.Error(c, 400, err.Error())
 	}
 
-	userDetails, err := u.Service.Get(&userUUID)
+	userDetails, err := u.UserService.Get(userUUID)
 	if err != nil {
 		return response.Error(c, 500, err.Error())
 	}
-	return response.Success(c, 200, userDetails)
+	userProperty, err := u.PropertyService.GetByOwnerID(userUUID)
+	if err != nil {
+		return response.Error(c, 500, err.Error())
+	}
+	userDetails.Property = userProperty
+
+	return response.Success(c, 200, userDetails, "")
 }
 
 func (u *User) Create(c *fiber.Ctx) error {
@@ -42,8 +65,8 @@ func (u *User) Create(c *fiber.Ctx) error {
 		return response.Error(c, 400, err.Error())
 	}
 
-	if err := u.Service.Create(&body.Name, &body.Email, &body.Avatar); err != nil {
+	if err := u.UserService.Create(body.Name, body.Email, body.Avatar); err != nil {
 		return response.Error(c, 500, err.Error())
 	}
-	return response.Success(c, 201, "User created successfully")
+	return response.Success(c, 201, nil, "User created successfully")
 }
