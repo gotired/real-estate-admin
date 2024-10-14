511ba56042d8faba956ab382fbcba05d8932c175
gotired <gotired@hotmail.com>
2024-10-14 14:08:31 +0700

    refactor: property model

--- server/internal/domain/model/user/base.go
+++ server/internal/domain/model/user/base.go
@@ -2,13 +2,13 @@ package user
 
 import (
 	"github.com/google/uuid"
-	"github.com/gotired/real-estate-admin/server/internal/domain/model"
+	"github.com/gotired/real-estate-admin/server/internal/domain/model/property"
 )
 
 type Base struct {
-	ID       uuid.UUID        `json:"id"`
-	Name     string           `json:"name"`
-	Email    string           `json:"email"`
-	Avatar   string           `json:"avatar"`
-	Property []model.Property `json:"property"`
+	ID       uuid.UUID           `json:"id"`
+	Name     string              `json:"name"`
+	Email    string              `json:"email"`
+	Avatar   string              `json:"avatar"`
+	Property []property.Property `json:"property"`
 }
