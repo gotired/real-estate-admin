7aac2b1714c8243e4ea68748a698c717679e4aaf
gotired <gotired@hotmail.com>
2024-10-14 14:17:22 +0700

    feat: add fine user by email in persistance

--- server/internal/infrastructure/persistance/user/impl.go
+++ server/internal/infrastructure/persistance/user/impl.go
@@ -4,58 +4,40 @@ import (
 	"database/sql"
 
 	"github.com/google/uuid"
-	"github.com/gotired/real-estate-admin/server/internal/domain/model"
-	userSchma "github.com/gotired/real-estate-admin/server/internal/domain/model/user"
+	userSchema "github.com/gotired/real-estate-admin/server/internal/domain/model/user"
 	"github.com/gotired/real-estate-admin/server/internal/domain/repository"
 )
 
-type UserRespositoryImpl struct {
+type initialize struct {
 	db *sql.DB
 }
 
 func New(db *sql.DB) repository.User {
-	return &UserRespositoryImpl{
+	return &initialize{
 		db: db,
 	}
 }
 
-func (r *UserRespositoryImpl) Get(id *uuid.UUID) (*userSchma.Base, error) {
-	var user userSchma.Base
+func (r *initialize) Get(id uuid.UUID) (*userSchema.Base, error) {
+	var user userSchema.Base
 	err := r.db.QueryRow("SELECT id, name, email, avatar FROM user_collection WHERE id = $1", id).
 		Scan(&user.ID, &user.Name, &user.Email, &user.Avatar)
 	if err != nil {
 		return nil, err
 	}
-
-	rows, err := r.db.Query("SELECT id, owner, title, description, type, location, price, photo FROM property_collection WHERE owner = $1", id)
-	if err != nil {
-		return nil, err
-	}
-	defer rows.Close()
-
-	var properties []model.Property
-	for rows.Next() {
-		var property model.Property
-		if err := rows.Scan(&property.ID, &property.Owner, &property.Title, &property.Description, &property.Type, &property.Location, &property.Price, &property.Photo); err != nil {
-			return nil, err
-		}
-		properties = append(properties, property)
-	}
-	user.Property = properties
-
 	return &user, nil
 }
 
-func (r *UserRespositoryImpl) List() ([]userSchma.Base, error) {
-	rows, err := r.db.Query("SELECT id, name, email, avatar FROM user_collection")
+func (r *initialize) List(skip *int, limit *int) ([]userSchema.Base, error) {
+	rows, err := r.db.Query("SELECT id, name, email, avatar FROM user_collection LIMIT $1 OFFSET $2", limit, skip)
 	if err != nil {
 		return nil, err
 	}
 	defer rows.Close()
 
-	var users []userSchma.Base
+	var users []userSchema.Base
 	for rows.Next() {
-		var user userSchma.Base
+		var user userSchema.Base
 		if err := rows.Scan(&user.ID, &user.Name, &user.Email, &user.Avatar); err != nil {
 			return nil, err
 		}
@@ -64,7 +46,7 @@ func (r *UserRespositoryImpl) List() ([]userSchma.Base, error) {
 	return users, nil
 }
 
-func (r *UserRespositoryImpl) Create(name *string, email *string, avatar *string) error {
+func (r *initialize) Create(name string, email string, avatar string) error {
 	_, err := r.db.Exec("INSERT INTO user_collection (id, name, email, avatar) VALUES ($1, $2, $3, $4)",
 		uuid.New(), name, email, avatar)
 
@@ -73,4 +55,14 @@ func (r *UserRespositoryImpl) Create(name *string, email *string, avatar *string
 	}
 
 	return nil
+}
+
+func (r *initialize) GetByEmail(email string) (*uuid.UUID, error) {
+	var user userSchema.Base
+	err := r.db.QueryRow("SELECT id, name, email, avatar FROM user_collection WHERE email = $1", email).
+		Scan(&user.ID, &user.Name, &user.Email, &user.Avatar)
+	if err != nil {
+		return nil, err
+	}
+	return &user.ID, nil
 }
