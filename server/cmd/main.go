c1b5a951c80ce644fd14894780edfa6161f29e17
gotired <gotired@hotmail.com>
2024-10-14 14:08:03 +0700

    feat: add property feature

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 
 	"github.com/gotired/real-estate-admin/server/internal/domain/service"
+	"github.com/gotired/real-estate-admin/server/internal/infrastructure/persistance/property"
 	"github.com/gotired/real-estate-admin/server/internal/infrastructure/persistance/user"
 	"github.com/gotired/real-estate-admin/server/internal/infrastructure/server"
 	"github.com/gotired/real-estate-admin/server/internal/interface/api/handler"
@@ -16,12 +17,18 @@ func main() {
 
 	userRepo := user.New(db)
 	userService := &service.User{Repository: userRepo}
-	userHandler := &handler.User{Service: userService}
+
+	propertyRepo := property.New(db)
+	propertyService := &service.Property{Repository: propertyRepo}
+
+	userHandler := &handler.User{UserService: userService, PropertyService: propertyService}
+	propertyHandler := &handler.Property{UserService: userService, PropertyService: propertyService}
 
 	apiRouter := app.Group("/api")
 	v1Router := apiRouter.Group("/v1")
 
 	router.SetupUserRouter(v1Router, userHandler)
+	router.SetupPropertyRouter(v1Router, propertyHandler)
 
 	log.Fatal(app.Listen(fmt.Sprintf(":%s", config.APP.Port)))
 }
