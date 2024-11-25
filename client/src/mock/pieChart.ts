bc46eb48153db8fd71795ae71106fb5b45386282
gotired <gotired@hotmail.com>
2024-11-25 21:04:20 +0700

    feat: add mock data

---
+++ client/src/mock/pieChart.ts
@@ -0,0 +1,28 @@
+const pieChartMock = [
+  {
+    title: "Properties for Sale",
+    value: 684,
+    series: [75, 25],
+    colors: ["#275be8", "#272B30"],
+  },
+  {
+    title: "Properties for Rent",
+    value: 550,
+    series: [60, 40],
+    colors: ["#FD8539", "#272B30"],
+  },
+  {
+    title: "Total customers",
+    value: 5684,
+    series: [75, 25],
+    colors: ["#2ED480", "#272B30"],
+  },
+  {
+    title: "Properties for Cities",
+    value: 555,
+    series: [75, 25],
+    colors: ["#FE6D8E", "#272B30"],
+  },
+];
+
+export default pieChartMock;
---
+++ client/src/mock/salesProperty.ts
@@ -0,0 +1,33 @@
+import image_1 from "@/assets/place_logo/image_1.png";
+import image_2 from "@/assets/place_logo/image_2.png";
+import image_3 from "@/assets/place_logo/image_3.png";
+import image_4 from "@/assets/place_logo/image_4.png";
+
+const salesCardMock = [
+  {
+    image: image_1,
+    name: "Metro Jayakar Apartment",
+    location: "North Carolina, USA",
+    price: "+$35",
+  },
+  {
+    image: image_2,
+    name: "Letdo Ji Hotel & Aportment",
+    location: "Carolina North, UK",
+    price: "+$40",
+  },
+  {
+    image: image_3,
+    name: "Star Sun Hotel & Apartment",
+    location: "North Carolina, USA",
+    price: "+$50",
+  },
+  {
+    image: image_4,
+    name: "Metro Jayakar Apartment",
+    location: "North Carolina, USA",
+    price: "+$45",
+  },
+];
+
+export default salesCardMock;
